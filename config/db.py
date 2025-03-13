e316da6be418413fc39d103b5e13259be08013c9
genes15 <andresgenes49@gmail.com>
2025-03-13 13:08:58 -0500

    cambios

--- config/db.py
+++ config/db.py
@@ -13,7 +13,7 @@ POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 # Servidor LINUX
 #SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db_ice:5432/${POSTGRES_DB}"
 
@@ -22,8 +22,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
 # Configuración del engine con SSL requerido
 engine = create_engine(
-    #SQLALCHEMY_DATABASE_URL
-    DATABASE_URL,
+    SQLALCHEMY_DATABASE_URL,
+    #DATABASE_URL,
     # connect_args={
     #     "sslmode": "require",
     # },
